e8e0c2e21b686c4a18ea2cfbb9e179b1c095e85e
Ahmed-028 <ahmedmagedmounir@gmail.com>
2026-06-24 17:59:19 +0300

    Add the base song code
    Provide a GetSongs in the SongController to return all songs in the provided test list.

---
+++ PlaylistManagerApi/Controllers/SongController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlaylistManagerApi.Models;
+
+namespace PlaylistManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongController : ControllerBase
+    {
+        //Using this to test functions before connecting database
+
+        static List<Song> testList = new List<Song> { 
+           new Song {Id = 1,Name="Sharks",Artist ="Imagine dragons",PublishDate= DateTime.Now},
+           new Song {Id = 2,Name="Beliver",Artist ="Imagine dragons",PublishDate= DateTime.Now},
+           new Song {Id = 3,Name="Radioactive",Artist ="Imagine dragons",PublishDate= DateTime.Now}
+
+        };
+
+        [HttpGet]
+
+        //Use async to prevent freezing
+        public async Task<ActionResult<List<Song>>> GetSongs()
+        {
+            return Ok(testList);
+        }
+    }
+}
---
+++ PlaylistManagerApi/Models/Song.cs
@@ -0,0 +1,10 @@
+﻿namespace PlaylistManagerApi.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Artist { get; set; } = string.Empty;
+        public DateTime PublishDate { get; set; }
+    }
+}
